dae19d94b456ed5995cf67ae14d6694bbe3f90ad
Vivek Joshi <vrj022@gmail.com>
2024-04-02 22:14:16 +0530

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'e_commerce',
+      entities: [],
+      synchronize: true,
+    }),
+    UserModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
