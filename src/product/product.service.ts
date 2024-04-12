c801beef23b969969c33474a1a0d3b189ff076ac
Vivek Joshi <vrj022@gmail.com>
2024-04-12 19:10:09 +0530

    Product filter functionality added.

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from '../entity/product.entity';
-import { Repository } from 'typeorm';
+import { Brackets, Repository } from 'typeorm';
 
 @Injectable()
 export class ProductService {
@@ -11,6 +11,34 @@ export class ProductService {
   ) {}
 
   async filter(req: any): Promise<any> {
-    return await this.productRepository.findOneBy({ name: req.name });
+    const productObj = this.productRepository.createQueryBuilder('product');
+    productObj
+      .leftJoinAndSelect('product.brand', 'brand')
+      .leftJoinAndSelect('product.category', 'category')
+      .leftJoinAndSelect('product.colors', 'color')
+      .where('product.name LIKE :name', { name: `%${req.name}%` });
+
+    if (req.brandId || req.categoryId || req.colorId) {
+      productObj.andWhere(
+        new Brackets((qb) => {
+          if (req.brandId) {
+            qb.where('brand.id IN (:...brandId)', {
+              brandId: req.brandId,
+            });
+          }
+          if (req.categoryId) {
+            qb.orWhere('category.id IN (:...categoryId)', {
+              categoryId: req.categoryId,
+            });
+          }
+          if (req.colorId) {
+            qb.orWhere('color.id IN (:...colorId)', {
+              colorId: req.colorId,
+            });
+          }
+        }),
+      );
+    }
+    return productObj.getMany();
   }
 }
