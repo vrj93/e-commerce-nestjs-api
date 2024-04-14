621f32101fe326827f9e36ce9d7cf229e77c9d8d
Vivek Joshi <vrj022@gmail.com>
2024-04-14 10:36:38 +0530

    Popular product by Brands API added for Dashboard.

--- src/dashboard/dashboard.service.ts
+++ src/dashboard/dashboard.service.ts
@@ -42,7 +42,29 @@ export class DashboardService {
     };
   }
 
-  getProductByBrand(): any {
+  async getProductByBrand(): Promise<any> {
+    const products = await this.productRepository
+      .createQueryBuilder('p')
+      .select('p.id', 'product_id')
+      .addSelect('p.name', 'product_name')
+      .addSelect('p.price', 'price')
+      .addSelect('b.name', 'brand')
+      .addSelect('b.rank', 'brand_rank')
+      .addSelect('c.name', 'category_name')
+      .addSelect('GROUP_CONCAT(c2.name)', 'colors')
+      .innerJoin(Brand, 'b', 'p.brandId = b.id AND b.rank != 0')
+      .innerJoin(Category, 'c', 'p.categoryId = c.id')
+      .innerJoin('p.colors', 'c2') //Color property -> Color table Alias
+      .where('p.brandRank = 1')
+      .groupBy('p.id')
+      .orderBy('b.rank')
+      .getRawMany();
 
+    return {
+      flag: true,
+      status: HttpStatus.OK,
+      msg: 'Product fetched successfully',
+      data: products,
+    };
   }
 }
