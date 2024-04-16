3c36109c716b41585f519a58b0931b8e60e13c87
Vivek Joshi <vrj022@gmail.com>
2024-04-16 22:19:26 +0530

    removed unnecessary code.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,7 +4,6 @@ import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../entity/user.entity';
 import { JwtModule } from '@nestjs/jwt';
-import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
