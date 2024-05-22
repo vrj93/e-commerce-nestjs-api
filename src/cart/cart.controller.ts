ac1f776ece711aee66230da41b7495687bca6356
Vivek Joshi <vrj022@gmail.com>
2024-05-22 14:23:09 +0530

    Cart Response structure optimized.

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,18 +1,24 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
 import { CartService } from './cart.service';
 import { AddToCartDto } from '../dto/add-to-cart.dto';
+import { Response } from 'express';
 
 @Controller('cart')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
 
   @Post('add')
-  async addToCart(@Body() addToCartReq: AddToCartDto): Promise<any> {
-    return this.cartService.addToCart(addToCartReq);
+  async addToCart(
+    @Body() addToCartReq: AddToCartDto,
+    @Res() res: Response,
+  ): Promise<any> {
+    const serviceRes = await this.cartService.addToCart(addToCartReq);
+    res.status(serviceRes.status).json(serviceRes);
   }
 
   @Get('user/:id')
-  async getCart(@Param() params: any): Promise<any> {
-    return this.cartService.getCart(params.id);
+  async getCart(@Param() params: any, @Res() res: Response): Promise<any> {
+    const serviceRes = await this.cartService.getCart(params.id);
+    res.status(serviceRes.status).json(serviceRes);
   }
 }
